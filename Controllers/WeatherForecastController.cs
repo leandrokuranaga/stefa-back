bb91d5df02a9dd3ab2e4d22b658dac66fe811738
Leandro Kuranaga <le.s.kuranaga@hotmail.com>
2023-11-28 16:01:33 -0300

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -26,7 +26,7 @@ namespace stefaninitest.Controllers
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)],
-                City = "Jaguariuna"
+                City = "Araçatuba"
                 
             })
             .ToArray();
